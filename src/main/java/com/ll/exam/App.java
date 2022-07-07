10cc9ac4b287f409e3107689af658f5793197c04
ahah525 <ahah525@naver.com>
2022-07-07 19:17:48 +0900

    등록기능 구현시작

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -14,6 +14,13 @@ public class App {
             String cmd = sc.nextLine().trim();  // 앞뒤 공백 제거
 
             switch (cmd) {
+                case "등록":
+                    System.out.print("명언 : ");
+                    String content = sc.nextLine().trim();   // 명언 입력받기
+                    System.out.print("작가 : ");
+                    String author = sc.nextLine().trim();   // 작가 입력받기
+                    System.out.println("1번 명언이 입력되었습니다.");
+                    break;
                 case "종료":
                     break outer ; // outer가 붙은 반복문을 break
             }
