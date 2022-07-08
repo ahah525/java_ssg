1629e3fe408e90ab55a50e76b1b3c32a0a3cf378
ahah525 <ahah525@naver.com>
2022-07-08 13:34:48 +0900

    WiseSaying 클래스 추가

---
+++ src/main/java/com/ll/exam/WiseSaying.java
@@ -0,0 +1,22 @@
+package com.ll.exam;
+
+public class WiseSaying {
+    int id;  // 명언글 번호
+    String content; // 명언
+    String author;   // 작가명
+
+    public WiseSaying(int id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "WiseSaying{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
