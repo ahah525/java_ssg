78597247f07c77a44ec9810df7461be63f1af3d1
ahah525 <ahah525@naver.com>
2022-07-08 17:12:49 +0900

    입력받은 명언 리스트에 저장, 목록기능 구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,15 +1,16 @@
 package com.ll.exam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     public void run() {
-        System.out.println("== 명언 SSG ==");
-
         Scanner sc = new Scanner(System.in);
-
+        List<WiseSaying> wiseSayings = new ArrayList<>();   // 명언을 저장하기 위한 리스트
         int wiseSayingLastId = 0;  // 가장 마지막 명언글의 번호
 
+        System.out.println("== 명언 SSG ==");
         outer:
         while (true) {
             System.out.print("명령) ");
@@ -22,11 +23,20 @@ public class App {
                     System.out.print("작가 : ");
                     String author = sc.nextLine().trim();   // 작가 입력받기
                     int id = ++wiseSayingLastId;   // 명언 글 번호 1 증가
-                    // WiseSaying 객체 생성
+                    // WiseSaying 객체 생성후 리스트에 저장
                     WiseSaying wiseSaying = new WiseSaying(id, content, author);
-                    System.out.println(wiseSaying);
-
+                    wiseSayings.add(wiseSaying);
                     System.out.printf("%d번 명언이 입력되었습니다.\n", id);
+                    //System.out.println(wiseSaying);
+                    break;
+                case "목록":
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("-----------------");
+                    // 역순으로 출력(최신순)
+                    for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+                        WiseSaying ws = wiseSayings.get(i);
+                        System.out.printf("%d / %s / %s\n", ws.id, ws.author, ws.content);
+                    }
                     break;
                 case "종료":
                     break outer; // outer가 붙은 반복문을 break
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -2,8 +2,7 @@ package com.ll.exam;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,7 +15,10 @@ public class AppTest {
     }
 
     @Test
-    public void 테스트_스캐너() {
+    public void 문자열을_스캐너의_입력으로_설정() {
+        // '\n' 대신 엔터 허용
+        // stripIndent(): 각 줄의 앞 공백 제거
+        // 키보드 입력 자동화
         String input = """
                 등록
                 명언1
@@ -24,7 +26,7 @@ public class AppTest {
                 """.stripIndent();
         InputStream in = new ByteArrayInputStream(input.getBytes());
         Scanner sc = new Scanner(in);
-
+        // trim(): 앞뒤 공백 제거
         String cmd = sc.nextLine().trim();
         String content = sc.nextLine().trim();
         String author = sc.nextLine().trim();
@@ -32,5 +34,21 @@ public class AppTest {
         assertEquals(cmd, "등록");
         assertEquals(content, "명언1");
         assertEquals(author, "작가1");
+    }
+    @Test
+    public void 표준출력을_리다이렉션하여_결과를_문자열로_받기() throws IOException {
+        // 표준출력을 리다이렉션
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        System.out.println("안녕");
+
+        String rs = output.toString().trim();
+
+        // 표준출력을 원상복구
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        output.close();
+
+        assertEquals("안녕", rs);
     }
 }
