8f94eab0c756214d429539179d5979d9eb6d5713
ahah525 <ahah525@naver.com>
2022-07-11 16:03:25 +0900

    Rq 클래스 리팩토링

--- src/main/java/com/ll/exam/Rq.java
+++ src/main/java/com/ll/exam/Rq.java
@@ -1,19 +1,28 @@
 package com.ll.exam;
 
 public class Rq {
-    private String url;
+    private String url;     // path + query 문
+    private String path;
+    private String queryStr;
 
     public Rq(String url) {
         this.url =  url;
+        String[] urlBits = url.split("\\?", 2);// ? 기준으로 2개로 분리
+        this.path = urlBits[0]; // path
+        // query 문이 있으면 저장
+        if(urlBits.length == 2) {
+            this.queryStr = urlBits[1];
+        }
     }
 
     // url의 파라미터에 해당하는 값 반환
     public int getIntParam(String paramName, int defaultValue) {
-        String[] urlBits = url.split("\\?", 2); // ? 기준으로 2개로 분리
-        urlBits = urlBits[1].split("&"); // &기준으로 분리
+        // query문이 null이면 디폴트값 반환
+        if(queryStr == null) return defaultValue;
 
-        for(String urlBit : urlBits) {
-            String[] param = urlBit.split("=", 2);// = 기준으로 2개로 분리
+        String[] queryBits = queryStr.split("&");// &기준으로 분리
+        for(String queryBit : queryBits) {
+            String[] param = queryBit.split("=", 2);// = 기준으로 2개로 분리
             String name = param[0];     // 파라미터명
             String value = param[1];    // 값
             // 해당 명령어에 대한 값 반환
@@ -24,9 +33,7 @@ public class Rq {
         return defaultValue;
     }
 
-    // url의 Path를 반환
     public String getPath() {
-        String[] urlBits = url.split("\\?", 2); // ? 기준으로 2개로 분리
-        return urlBits[0];  // ? 앞에 있는 값이 path
+        return path;
     }
 }
